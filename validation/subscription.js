27701edd7be5b2c21ab9b0df32caa44b3f03f51d
Akash Yadav <akashyadav@bizzeonline.com>
2023-04-12 12:37:36 +0530

    update

--- validation/subscription.js
+++ validation/subscription.js
@@ -30,6 +30,7 @@ exports.id = Joi.object().keys({
     id:Joi.string().regex(/^[0-9a-fA-F]{24}$/).message('must be an oid').required(),
 });
 
+
 exports.filters = Joi.object().keys({
     pageSize:Joi.string().optional().default(500),
     pageNo:Joi.string().optional().default(1),
